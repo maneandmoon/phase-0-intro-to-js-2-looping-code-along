7ab3e9d3a1cc0fda0ee08d67d81589d9a586f076
LindaTa <lindata@maneandmoon.com>
2024-06-17 16:32:32 -0500

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+      }
+    return messages;
+}
+
+
+const countDown = (number) => {
+    console.log(number);
+  
+    if (number === 0) {
+      return;
+    } else {
+      countDown(number - 1);
+    }
+  };
+  
+  countDown(10);
